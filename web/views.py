fcda0e0153fb8ebc9ba970d77f1841033334c118
JoHyorim <1120bear@naver.com>
2022-06-20 22:22:04 +0900

    4 commit

--- web/views.py
+++ web/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("JoHyorim_Final")
+    context = {
+
+    }
+
+    return render(request, "index.html", context = context)
